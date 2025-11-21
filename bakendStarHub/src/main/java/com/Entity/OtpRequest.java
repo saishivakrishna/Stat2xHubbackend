a96481d4a3be14afc8b1ed660c6f75422c9fc7f8
saishivakrishna <saishivakrishna2018@gmail.com>
2025-11-21 22:16:04 +0530

    Create OtpRequest.java

---
+++ bakendStarHub/src/main/java/com/Entity/OtpRequest.java
@@ -0,0 +1,13 @@
+package com.Entity;
+
+public class OtpRequest {
+    private String phone;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
