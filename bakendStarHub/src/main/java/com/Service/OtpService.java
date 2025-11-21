fd94524ddacddbd7a9e42c621868c09aa2d1f8e2
saishivakrishna <saishivakrishna2018@gmail.com>
2025-11-21 22:11:45 +0530

    Add package declaration to OtpService.java

--- bakendStarHub/src/main/java/com/Service/OtpService.java
+++ bakendStarHub/src/main/java/com/Service/OtpService.java
@@ -1,3 +1,5 @@
+package com.Service;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
