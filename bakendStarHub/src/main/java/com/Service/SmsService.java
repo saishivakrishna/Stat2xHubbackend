bcd52a7ae40db7766df500bad7d537bbbd2b7a9b
saishivakrishna <saishivakrishna2018@gmail.com>
2025-11-23 11:21:20 +0530

    Implement SmsService for sending OTP messages

---
+++ bakendStarHub/src/main/java/com/Service/SmsService.java
@@ -0,0 +1,27 @@
+package com.app.service;
+
+import org.springframework.stereotype.Service;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+
+@Service
+public class SmsService {
+
+    private final String ACCOUNT_SID = "ACb154e313ca430673dfe1fe9a66fe6987";
+    private final String AUTH_TOKEN = "0df134ca59489c710ce33c792482542d";
+    private final String TWILIO_NUMBER = "+13312002813";   // Twilio number
+
+    public SmsService() {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+    }
+
+    public String sendOtp(String to, String otp) {
+        Message message = Message.creator(
+                new com.twilio.type.PhoneNumber("+91" + to),
+                new com.twilio.type.PhoneNumber(TWILIO_NUMBER),
+                "Your OTP is: " + otp)
+                .create();
+
+        return message.getSid();
+    }
+}
